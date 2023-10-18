a2ee7ce82da9a516246d0d45831d106684287387
Belen Torres <belentorreshuanque@gmail.com>
2023-10-18 18:06:55 -0500

    v.04 add AppForm

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Contador2 from './components/C03componente';
 import Contador3 from './components/C04componente';
 import C05operador from './components/C05operadorTemario';
 import C06matriz from './components/C06matriz1';
-
+import { AppForm } from './components/AppForm';
 
 function App() {
   return (
-    <div>
-      <h1></h1>
-      <C01componente/> 
-      <hr/>
+    <div style={{display:"grid", placeItems:"center", width:"", background:"white", padding:"20px"}}>
+      
+      <AppForm />
+      <p>Nº 1 Juan Manuel            x    A</p>
+      <p>Nº 2 Maria Valencia            x    A</p>
     </div>
   );
 }
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,16 +1,17 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-var firebaseConfig = {
-    apiKey: "AIzaSyARtSt-W9YENYdkSe-3okjGI6192j-J0I0",
-    authDomain: "fb-bdreact01-5c82d.firebaseapp.com",
-    projectId: "fb-bdreact01-5c82d",
-    storageBucket: "fb-bdreact01-5c82d.appspot.com",
-    messagingSenderId: "826697868382",
-    appId: "1:826697868382:web:455194748e0eadaac740cf"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARtSt-W9YENYdkSe-3okjGI6192j-J0I0",
+  authDomain: "fb-bdreact01-5c82d.firebaseapp.com",
+  projectId: "fb-bdreact01-5c82d",
+  storageBucket: "fb-bdreact01-5c82d.appspot.com",
+  messagingSenderId: "826697868382",
+  appId: "1:826697868382:web:364c62783dc864dbc740cf"
 };
 
-const fb = firebase.initializeApp(firebaseConfig);
-
-export const bd = fb.firestore();
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
